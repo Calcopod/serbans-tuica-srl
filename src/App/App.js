4443b89221c61f45c6182a631bc3720f47e032a3
= <=>
2021-04-21 20:56:19 +0300

    Implemented basic routing.

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './App.css';
 import HomePage from '../Pages/Homepage/homepage.jsx';
+
+import { Switch, Route } from 'react-router-dom'
+
+import './App.css';
+
+const HatsPage = () => (
+  <h1>Hat page</h1>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+     <Switch>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path="/hats" >
+        <HatsPage />
+      </Route>
+     </Switch>
     </div>
   );
 }
