cd389d66ac6bccc968fc17aeafdbc516d97371da
= <=>
2021-04-24 18:24:13 +0300

    Header component + Routing via said comp.

--- src/App/App.js
+++ src/App/App.js
@@ -5,18 +5,20 @@ import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
 import ShopPage from '../Pages/Shop/shop.jsx';
+import Header from '../Components/header/header.jsx';
 
 function App() {
   return (
     <div>
-     <Switch>
-      <Route exact path="/">
-        <HomePage />
-      </Route>
-      <Route path="/shop" >
-        <ShopPage />
-      </Route>
-     </Switch>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/shop" >
+          <ShopPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
